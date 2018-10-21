f060e387f6552889f0c79cb525e4501d81747d09
Patrick Chan <patrickchan1999@gmail.com>
2018-10-20 21:46:23 -0400

    Add task list: container for all tasks on the page

---
+++ web/components/TaskList/TaskList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <task v-for="task in this.tasks" :task="task">
+    </task>
+  </div>
+</template>
+
+<script>
+import Task from 'Task/Task.vue';
+
+export default {
+    name: 'TaskList',
+    components: {Task},
+    data: () => ({
+    }),
+    methods: {
+    },
+    props: ['tasks'],
+    watch: {
+        tasks(newValue, oldValue) {
+            console.log(newValue);
+        },
+    },
+}
+</script>
